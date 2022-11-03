e07ae57ac1daf6a68282efe24644987be812f72b
Your Name <mpnirush.1810@gmail.com>
2022-11-03 16:19:41 +0530

    models, routes finished

--- backend/index.js
+++ backend/index.js
@@ -3,53 +3,30 @@ const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
+app.use(cors());
+app.options("*", cors());
 
 app.use(bodyParser.json());
 app.use(morgan("tiny"));
 
-const productSchema = mongoose.Schema({
-  name: String,
-  image: String,
-  countInStock: {
-    type: Number,
-    required: true,
-  },
-});
-
-const Product = mongoose.model("Product", productSchema);
-
 require("dotenv/config");
+
+// const categoriesRoutes = require("./routes/categories");
+const productsRoutes = require("./routes/products");
+// const usersRoutes = require("./routes/users");
+// const ordersRoutes = require("./routes/orders");
 
 const api = process.env.API_URL;
 
-app.get(`${api}/products`, async (req, res) => {
-  const productList = await Product.find();
+// app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+// app.use(`${api}/users`, usersRoutes);
+// app.use(`${api}/orders`, ordersRoutes);
 
-  if (!productList) {
-    res.status(500).json({ success: false });
-  }
-  res.send(productList);
-});
-
-app.post(`${api}/products`, (req, res) => {
-  const product = new Product({
-    name: req.body.name,
-    image: req.body.image,
-    countInStock: req.body.countInStock,
-  });
-
-  product.save().then((createdProduct) => {
-      res.status(201).json(createdProduct);
-    })
-    .catch((err) => {
-      res.status(500).json({
-        error: err,
-        success: false,
-      });
-    });
-});
-
-mongoose.connect(process.env.CONNECTION_STRING, {
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     dbName: "e-app",
